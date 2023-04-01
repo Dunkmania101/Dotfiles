7c8c48443ce9993154ff601b56a0722283b4d5e9
Dunkmania101 <50186904+Dunkmania101@users.noreply.github.com>
2023-04-01 17:58:55 -0400

    Sync configs

--- user_home/.config/qtile/autostart.sh
+++ user_home/.config/qtile/autostart.sh
@@ -26,8 +26,8 @@ playerctld daemon &
 #deadd-notification-center &
 pgrep copyq || copyq &
 #pgrep synapse || synapse --startup &
-#pgrep element-desktop || element-desktop --hidden &
-pgrep element-desktop-nightly || element-desktop-nightly --hidden &
+pgrep element-desktop || element-desktop --hidden &
+#pgrep element-desktop-nightly || element-desktop-nightly --hidden &
 #pgrep birdtray || birdtray &
 pgrep easyeffects || easyeffects --gapplication-service &
 #agordejo --hide --load-session Main &
