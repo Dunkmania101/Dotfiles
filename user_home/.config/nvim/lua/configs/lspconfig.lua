cbe182efaf09ba26165a2943830c0be86b49b9a5
Dunkmania101 <50186904+Dunkmania101@users.noreply.github.com>
2024-11-04 18:40:21 -0500

    Sync Config

--- user_home/.config/nvim/lua/configs/lspconfig.lua
+++ user_home/.config/nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "basedpyright", "jdtls" }
+local servers = { "html", "cssls", "basedpyright", "jdtls", "clangd" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
