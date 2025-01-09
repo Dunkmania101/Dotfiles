552f85d556deac3d5b3704ca82bc93f393a3ae68
Dunkmania101 <50186904+Dunkmania101@users.noreply.github.com>
2025-01-08 22:02:50 -0500

    Sync Configs

---
+++ init-chaotic-aur.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key 3056513887B78AEB
+
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+
+sudo echo '
+[chaotic-aur]
+Include = /etc/pacman.d/chaotic-mirrorlist' >> /etc/pacman.conf
+
+sudo pacman -Syu
+
