154555c3958c704b72ef9669cd277b09e8cd682b
Dunkmania101 <50186904+Dunkmania101@users.noreply.github.com>
2025-03-27 11:04:49 -0400

    Sync Configs

--- user_home/.config/nvim/lua/plugins/init.lua
+++ user_home/.config/nvim/lua/plugins/init.lua
@@ -1,25 +1,33 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
-  },
+    {
+        "stevearc/conform.nvim",
+        -- event = 'BufWritePre', -- uncomment for format on save
+        opts = require "configs.conform",
+    },
 
-  -- These are some examples, uncomment them if you want to see them work!
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end,
-  },
+    -- These are some examples, uncomment them if you want to see them work!
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            require "configs.lspconfig"
+        end,
+    },
 
-  {
-  	"nvim-treesitter/nvim-treesitter",
-  	opts = {
-  		ensure_installed = {
-  			"vim", "lua", "vimdoc",
-       "html", "css"
-  		},
-  	},
-  },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = {
+            ensure_installed = {
+                "vim", "lua", "vimdoc",
+                "html", "css"
+            },
+        },
+    },
+    {
+        'vimwiki/vimwiki',
+        lazy = false,
+        init = function () --replace 'config' with 'init'
+            vim.g.vimwiki_list = {{path = '~/superdrive-ln/Docs/VimWiki', syntax = 'markdown', ext = '.md'}}
+        end
+    },
 }
+
