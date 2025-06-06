55b30245f4424ae0b4489076a9e278980fc39ceb
Dunkmania101 <50186904+Dunkmania101@users.noreply.github.com>
2025-06-06 16:47:31 -0400

    Sync Configs

--- user_home/.config/nvim/lua/options.lua
+++ user_home/.config/nvim/lua/options.lua
@@ -3,7 +3,7 @@ require "nvchad.options"
 -- add yours here!
 
 local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+o.cursorlineopt = 'both' -- to enable cursorline!
 
 o.spelllang = 'en_us'
 o.spell = true
